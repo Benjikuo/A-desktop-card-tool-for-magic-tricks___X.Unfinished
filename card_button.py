8eb943a0cf912102e893af374d9eecf7f89fa3ef
unknown <benjaminkuo941031@gmail.com>
2025-10-24 12:47:35 +0800

    add basic card fuction

---
+++ card_button.py
@@ -0,0 +1,62 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import subprocess
+
+WHITE_IMG = "./image/button/card_button_white.png"
+GRAY_IMG = "./image/button/card_button_gray.png"
+ORANGE_IMG = "./image/button/card_button_orange.png"
+TARGET_SCRIPT = "cards.py"
+BG_COLOR = "#000000"
+SCALE = 0.52
+card_program = None
+
+
+def toggle_cards(event=None):
+    global card_program
+    if card_program and card_program.poll() is None:
+        label.config(image=photo_white)
+        print("🟥 close cards.py")
+        card_program.terminate()
+        card_program = None
+    else:
+        label.config(image=photo_orange)
+        print("🟩 open cards.py")
+        card_program = subprocess.Popen(["python", TARGET_SCRIPT])
+
+
+def press_in(event):
+    label.config(image=photo_gray)
+
+
+def press_out(event):
+    toggle_cards()
+
+
+root = tk.Tk()
+root.overrideredirect(True)
+root.config(bg=BG_COLOR)
+root.wm_attributes("-transparentcolor", BG_COLOR)
+root.geometry("+142+697")
+
+
+img_white = Image.open(WHITE_IMG)
+img_gray = Image.open(GRAY_IMG)
+img_orange = Image.open(ORANGE_IMG)
+
+if SCALE != 1.0:
+    w, h = img_white.size
+    img_white = img_white.resize((int(w * SCALE), int(h * SCALE)))
+    img_gray = img_gray.resize((int(w * SCALE), int(h * SCALE)))
+    img_orange = img_orange.resize((int(w * SCALE), int(h * SCALE)))
+
+photo_white = ImageTk.PhotoImage(img_white)
+photo_gray = ImageTk.PhotoImage(img_gray)
+photo_orange = ImageTk.PhotoImage(img_orange)
+
+
+label = tk.Label(root, image=photo_white, bg=BG_COLOR, bd=0)
+label.pack()
+
+label.bind("<ButtonPress-1>", press_in)
+label.bind("<ButtonRelease-1>", press_out)
+root.mainloop()
