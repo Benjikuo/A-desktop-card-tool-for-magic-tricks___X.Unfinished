25f2acb7bc4fa310d134eae51bb8c61ed019f7cb
unknown <benjaminkuo941031@gmail.com>
2025-11-02 20:41:04 +0800

    clean the file

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-"s": lambda: focus_box.spawn_spread(delete_used=ctrl, face_up=shift),
-            # "w": lambda: focus_box.spawn_spread(
-            #     delete_used=ctrl, face_up=shift, sort="standard"
-            # ),
